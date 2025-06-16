6ba40d02a973ad2ba67171df91022ee520bd6edb
Jotam Combe <jotamc@wix.com>
2025-06-16 18:58:46 +0300

    Fix formatting in type signature comment for equal-trees function in ex4.rkt

--- ex4.rkt
+++ ex4.rkt
@@ -59,7 +59,7 @@
 
 ;;; Q3.2
 ; Signature: equal-trees$(tree1, tree2, succ, fail) 
-; Type: [Tree * Tree * [Tree ->T1] * [Pair->T2] -> T1 U T2
+; Type: [Tree * Tree * [Tree ->T1] * [Pair->T2] -> T1 U T2]
 ; Purpose: Determines the structure identity of a given two lists, with post-processing succ/fail
 (define equal-trees$ 
   (lambda (tree1 tree2 succ fail)
@@ -110,6 +110,7 @@
       (lambda () (** (tail x) (tail y))))
   )
 )
+
 ;; Signature: //(x, y)
 ;; Type: [ Lzl(Number) * Lzl(Number) -> Lzl(Number) ]
 ;; Purpose: Division of real numbers
